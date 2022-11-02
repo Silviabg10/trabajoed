bdfd37bdeeedcadd71fe8893bc4224727e8d0dcd
Daniel <drodenaslagares@gmail.com>
2022-11-02 13:20:31 +0100

    fldksjflskdj

--- PEP1T_2_SBG.py
+++ PEP1T_2_SBG.py
@@ -20,3 +20,6 @@ precio = (0.50 * numelen) + (0.25 * numelen2)
 print("La cadena contiene", len(frase), "palabras, de las cuales", numelen, "tienen más de 5 letras.")
 print("Por tanto, al precio de 0.25€/palabra tenemos ", numelen2, "y a 0.50€/palabra hay otras", numelen, ".")
 print(f"Total: {precio}€")
+
+
+# Hola caracola patimi cocacola
